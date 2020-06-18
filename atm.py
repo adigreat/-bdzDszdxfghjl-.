b3ec59ad5f9bb829ecbbd0004e595fbd348429fb
adigreat <53363766+adigreat@users.noreply.github.com>
2020-06-18 22:12:23 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,20 @@
+
+class atm:
+    def __init__(self,ATMpin ,ATMnumber):
+        self.ATMnumber=ATMnumber
+        self.ATMpin=ATMpin
+
+def start(self):
+    print("started")
+
+def stop(self):
+    print("stop")
+
+def main():
+    cardNumber=input("enter you card number")
+    cardPin=input("enter the pin") 
+
+newuser=atm(ATMnumber,ATMpin)    
+
+
+activity=input("press 1 for balance check and 2 for withdrawl")
